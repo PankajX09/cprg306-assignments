0dbfb96a04e1b70af3804dfa3171f9d60765fe51
pankaj panchal <pankajkunjalkumar.panchal@edu.sait.ca>
2025-09-19 16:44:16 -0600

    edited code

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Pankaj Panchal</p>
+      <p>
+        <Link href="https://github.com/PankajX09/cprg306-assignments">
+          Pnakaj's GitHub
+        </Link>
+      </p>
+    </div>
+  );
+}

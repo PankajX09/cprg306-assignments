cd850bce817d4267afd509d44488e7d1d1a149ac
pankaj panchal <pankajkunjalkumar.panchal@edu.sait.ca>
2025-09-23 11:41:49 -0600

    updates

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <div>
-      <p>Pankaj Panchal</p>
+      <p>Pankaj Panchal, Alex Jamal, Kevin Kim</p>
       <p>
         <Link href="https://github.com/PankajX09/cprg306-assignments">
           Assignment&apos;s GitHub Repository

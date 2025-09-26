a9454abd7a0cad114d87c2bf1c6dc74f888de859
Alex Jamal <alex88jamal@gmail.com>
2025-09-26 17:20:39 -0600

    added week 3

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="max-w-xl mx-auto p-6">
+      <h1 className="text-4xl font-bold">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}

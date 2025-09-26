73b6193be395f228c8e166ae293197b396ae713c
Alex Jamal <alex88jamal@gmail.com>
2025-09-26 17:29:42 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ export default function Page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="/week-2">Go to Week 2 Page</Link>
+        <br></br>
+        <Link href="/week-3">Go to Week 3 Page</Link>
       </p>
       <p>Group 1: Pankaj Panchal, Alex Jamal, Kevin Kim</p>
     </main>

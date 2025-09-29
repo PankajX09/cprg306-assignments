11293a986c0497f82cd2b193641f8eb41bc2777a
badoobob <alex88jamal@gmail.com>
2025-09-29 12:24:33 -0600

    week 4
    created week 4 folders/files and removed some unnecessary code from week 3 items.js

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ export default function Page() {
         <Link href="/week-2">Go to Week 2 Page</Link>
         <br></br>
         <Link href="/week-3">Go to Week 3 Page</Link>
+        <br></br>
+        <Link href="/week-4">Go to Week 4 Page</Link>
       </p>
       <p>Group 1: Pankaj Panchal, Alex Jamal, Kevin Kim</p>
     </main>

9e98e05ab12287bca2e1d245056b25414f5c1fe7
pankaj panchal <pankajkunjalkumar.panchal@edu.sait.ca>
2025-10-13 18:58:00 -0600

    page.js

--- app/page.js
+++ app/page.js
@@ -4,18 +4,18 @@ export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
+      <div>
         <Link href="/week-2">Go to Week 2 Page</Link>
-        <br></br>
+        <br />
         <Link href="/week-3">Go to Week 3 Page</Link>
-        <br></br>
+        <br />
         <Link href="/week-4">Go to Week 4 Page</Link>
-        <br></br>
+        <br />
         <Link href="/week-5">Go to Week 5 Page</Link>
-        <br></br>
+        <br />
         <Link href="/week-6">Go to Week 6 Page</Link>
-        <br></br>
-      </p>
+        <br />
+      </div>
       <p>Group 1: Pankaj Panchal, Alex Jamal, Kevin Kim</p>
     </main>
   );
